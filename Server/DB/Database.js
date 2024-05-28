4910887eb01ed2dcd7176b7b52f7bd94dfc36801
mohammad shaheer <shaheercse2022@gmail.com>
2024-05-28 15:09:28 +0530

    final commit last

--- Server/DB/Database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URL, {}).then((data) => {
-    console.log(`MongoDb connected with server : ${data.connection.host}`);
-  });
-};
-
-module.exports = connectDatabase;
--- Server/Middleware/not-Found.js
+++
@@ -1,5 +0,0 @@
-const notFound = (error, req, res, next) => {
-  res.status(404).json({ message: "Requested route does not exist" });
-};
-
-module.exports = notFound;
